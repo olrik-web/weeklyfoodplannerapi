123dc1bf703952079c17948b20118a9bfbdc36c8
Marcus <molrik@outlook.com>
2022-08-13 19:27:26 +0200

    Commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("Hello world");
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -13,13 +11,21 @@ app.get("/", (req, res) => {
 });
 
 app.use(require("./routes/record"));
+app.get("/", (req, res) => {
+  res.send("Record is running");
+});
 // get driver connection
 const dbo = require("./db/conn");
-
+app.get("/", (req, res) => {
+  res.send("DB is running");
+});
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
   });
   console.log(`Server is running on port: ${port}`);
+});
+app.get("/", (req, res) => {
+  res.send("Everything is running");
 });
